eff2014af00241983f8a27eacbf9a0f27bdd1603
David Avery <eismausen@gmail.com>
2021-12-18 18:49:16 -0800

    commit tree 4bed1070a1d9c3b3934a29a444ea6568cdca86ec

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+
+    rescue_from ActiveRecord::RecordInvalid, with: :rescue_record_invalid
+
+    private
+
+    def rescue_record_invalid(exception)
+        error_hash = {}
+        exception.record.errors.each do |error|
+            error_hash[error.attribute] = error.full_message
+        end
+        render json: { errors: error_hash }, status: :unprocessable_entity
+    end
 end
